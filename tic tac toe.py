3f98e896c4f3b79410be8e0516ee7b19d3470fc3
Kowzhik <kowzhik.gs.dev@gmail.com>
2026-02-12 15:09:09 +0530

    done few things

--- tic tac toe.py
+++ tic tac toe.py
@@ -14,7 +14,9 @@ c3=0
 l = ["a1","a2","a3","b1","b2","b3","c1","c2"]
 def board_logic():
     print("  1    2    3")
+    global board
     global board_map
+    board = {"a1":"a1","a2":"a2","a3":"a3","b1":"b1","b2":"b2","b3":"b3","c1":"c1","c2":"c2","c3":"c3"}
     board_map= f"a {a1}  | {a2}  | {a3} \n\nb {b1}  | {b2}  | {b3} \n\nc {c1}  | {c2}  | {c3} "
     return board_map
 
@@ -22,9 +24,8 @@ print(board_logic())
 
 userinput= input("Enter where to change:")
 
-
-if userinput == "a1":
-    a1 ="x"
-    print(board_logic())
-
-
+for x in l:
+    if userinput == x:
+        board[str(userinput)]="x "
+        board_map= f" {board["a1"]}  | {board["a2"]} | {board["a3"]} \n\n {board["b1"]}  | {board["b2"]} | {board["b3"]} \n\n {board["c1"]}  | {board["c2"]} | {board["c3"]} "
+        print(board_map)
