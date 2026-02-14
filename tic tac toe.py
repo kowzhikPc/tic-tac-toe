d863d5be5f9bb994a96593b3d3d8a446fd880c9f
KowzhikPc <kowzhik.gs.dev@gmail.com>
2026-02-14 23:34:51 +0530

    All dimensions added

--- tic tac toe.py
+++ tic tac toe.py
@@ -2,6 +2,8 @@ import colorama
 from colorama import Fore,Back,Style
 colorama.init(autoreset=True)
 import sys
+import time
+
 a1 =0
 a2 =0
 a3=0
@@ -13,6 +15,11 @@ c2=0
 c3=0
 l = ["a1","a2","a3","b1","b2","b3","c1","c2","c3"]
 print(Fore.LIGHTBLUE_EX+Style.BRIGHT+"Hello Gamers!\n")
+def exit_logic():   
+    print("\n-->Game Restart!")
+    time.sleep(2)
+    sys.exit(0)
+
 def board_logic():
     print(Fore.LIGHTRED_EX +Style.BRIGHT+ "  1    2    3")
     global board
@@ -22,8 +29,10 @@ def board_logic():
     return board_map
 
 print(board_logic())
+all_inputs = []
 
 userinput_x= input("Enter where to change:")
+all_inputs.append(userinput_x)
 
 if userinput_x in l:
     board[str(userinput_x)]="x "
@@ -31,9 +40,10 @@ if userinput_x in l:
     print(board_map)
 else:
     print(Fore.LIGHTRED_EX+"\nInvalid Dimensions!")
-    sys.exit(0)
+    exit_logic()
 
 userinput_o= input("Enter where to change:")
+all_inputs.append(userinput_o)
 
 if userinput_o != userinput_x:
     if userinput_o in l:
@@ -42,5 +52,85 @@ if userinput_o != userinput_x:
         print(board_map)
     else:
         print("Invalid Dimensions!\n")
+        exit_logic()
 else:
     print("\nPosition alderdy used!")
+    exit_logic()
+
+userinput_x_2= input("Enter where to change:")
+all_inputs.append(userinput_x_2)
+
+if userinput_x_2 != userinput_x and userinput_o:
+    board[str(userinput_x_2)] = "x "
+    board_map= f" {board["a1"]}  | {board["a2"]} | {board["a3"]} \n\n {board["b1"]}  | {board["b2"]} | {board["b3"]} \n\n {board["c1"]}  | {board["c2"]} | {board["c3"]} "
+    print(board_map)
+
+else:
+    print("Invalid Dimensions")
+    exit_logic()
+
+userinput_o_2 =input("Enter where to change:")
+
+
+if userinput_o_2 not in all_inputs:
+    all_inputs.append(userinput_o_2)
+    board[str(userinput_o_2)] = "o "
+    board_map= f" {board["a1"]}  | {board["a2"]} | {board["a3"]} \n\n {board["b1"]}  | {board["b2"]} | {board["b3"]} \n\n {board["c1"]}  | {board["c2"]} | {board["c3"]} "
+    print(board_map)
+else:
+    print("invalid dimensions")
+    exit_logic()
+userinput_x_3 = input("Enter where to change:")
+
+if userinput_x_3 not in all_inputs:
+    all_inputs.append(userinput_x_3)
+    board[str(userinput_x_3)] = "x "
+    board_map= f" {board["a1"]}  | {board["a2"]} | {board["a3"]} \n\n {board["b1"]}  | {board["b2"]} | {board["b3"]} \n\n {board["c1"]}  | {board["c2"]} | {board["c3"]} "
+    print(board_map)
+else:
+    print("invalid Dimensions")
+    exit_logic()
+userinput_o_3 = input("Enter where to change:")
+
+if userinput_o_3 not in all_inputs:
+    all_inputs.append(userinput_o_3)
+    board[str(userinput_o_3)] = "o "
+    board_map= f" {board["a1"]}  | {board["a2"]} | {board["a3"]} \n\n {board["b1"]}  | {board["b2"]} | {board["b3"]} \n\n {board["c1"]}  | {board["c2"]} | {board["c3"]} "
+    print(board_map)
+else:
+    print("invalid dimensions")
+    exit_logic()
+
+userinput_x_4 =input("Enter where to change:")
+
+if userinput_x_4 not in all_inputs:
+    all_inputs.append(userinput_x_4)
+    board[str(userinput_x_4)] = "x "
+    board_map= f" {board["a1"]}  | {board["a2"]} | {board["a3"]} \n\n {board["b1"]}  | {board["b2"]} | {board["b3"]} \n\n {board["c1"]}  | {board["c2"]} | {board["c3"]} "
+    print(board_map)
+else:
+    print("invalid dimensions")
+    exit_logic()
+
+userinput_o_4 =input("Enter where to change:")
+
+if userinput_o_4 not in all_inputs:
+    all_inputs.append(userinput_o_4)
+    board[str(userinput_o_4)] = "o "
+    board_map= f" {board["a1"]}  | {board["a2"]} | {board["a3"]} \n\n {board["b1"]}  | {board["b2"]} | {board["b3"]} \n\n {board["c1"]}  | {board["c2"]} | {board["c3"]} "
+    print(board_map)
+else:
+    print("invalid dimensions")
+    exit_logic()
+
+userinput_x_5 =input("Enter where to change:")
+
+if userinput_x_5 not in all_inputs:
+    all_inputs.append(userinput_x_5)
+    board[str(userinput_x_5)] = "x "
+    board_map= f" {board["a1"]}  | {board["a2"]} | {board["a3"]} \n\n {board["b1"]}  | {board["b2"]} | {board["b3"]} \n\n {board["c1"]}  | {board["c2"]} | {board["c3"]} "
+    print(board_map)
+else:
+    print("invalid dimensions")
+    exit_logic()
+
